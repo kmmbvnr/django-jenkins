3eae862fb75f5d598daf2ca7f639a02a8b4d80a3
Chris Heisel <chris@heisel.org>
2010-07-19 14:22:28 -0400

    Added south support

--- django_hudson/management/commands/xmltest.py
+++ django_hudson/management/commands/xmltest.py
@@ -4,6 +4,17 @@ from optparse import make_option
 from django.core.management.base import BaseCommand
 from django_hudson.xmlrunner import XmlDjangoTestSuiteRunner
 
+try:
+    import south
+    try:
+        from south.management.commands import patch_for_test_db_setup
+    except Exception:
+        #Just in case the south paths change, we want to fail fast and loud
+        raise
+except ImportError:
+    #Must not have south
+    patch_for_test_db_setup = lambda x: pass
+
 class Command(BaseCommand):
     help = "Runs the test suite with reporting to xml"
 
@@ -14,6 +25,7 @@ class Command(BaseCommand):
 
 
     def handle(self, *test_labels, **options):
+        patch_for_test_db_setup()
         output_dir=options.get('output_dir')
         
         test_runner = XmlDjangoTestSuiteRunner(output_dir=output_dir)
@@ -21,4 +33,3 @@ class Command(BaseCommand):
 
         if failures:
             sys.exit(bool(failures))
-
