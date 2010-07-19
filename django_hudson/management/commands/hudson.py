a66e4a2e856afbf3b531898383461b74e5f8baf1
Chris Heisel <chris@heisel.org>
2010-07-19 17:50:15 -0400

    Removed lint, added south patch, made coverage handle individual apps

--- django_hudson/management/commands/hudson.py
+++ django_hudson/management/commands/hudson.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django_hudson.management.commands.lint import Command as pylint
 from django_hudson.xmlrunner import XmlDjangoTestSuiteRunner
+from django_hudson.management.commands.xmltest import patch_for_test_db_setup
 
 class Command(BaseCommand):
     help = "Run ci process"
@@ -14,43 +15,49 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--output', dest='output_dir', default='reports',
                     help='Reports directory'),
+        make_option('--noinput', action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.'),
     )
 
-    def handle(self, *args, **options):
+    def handle(self, *test_labels, **options):
         """
         Run pylint and test with coverage and xml reports
         """
+        patch_for_test_db_setup()
+
+        verbosity = int(options.get('verbosity', 1))
+        interactive = options.get('interactive', True)
+
         output_dir=options.get('output_dir')
         if not path.exists(output_dir):
             os.makedirs(output_dir)
 
-        app_labels = Command.app_list()
+        if not test_labels:
+            test_labels = Command.test_labels()
 
+        #TODO: Make lint work and with external rc file
         # pylint
-        pylint().handle(*app_labels, 
-                         output_file=path.join(output_dir,'pylint.report'))
-
+        #pylint().handle(*app_labels, 
+                         #output_file=path.join(output_dir,'pylint.report'))
 
         #coverage
         coverage.exclude('#pragma[: ]+[nN][oO] [cC][oO][vV][eE][rR]')
         coverage.start()
         
         #tests
-        test_runner = XmlDjangoTestSuiteRunner(output_dir=output_dir)
-        failures = test_runner.run_tests(app_labels)
+        test_runner = XmlDjangoTestSuiteRunner(output_dir=output_dir, interactive=interactive)
+        failures = test_runner.run_tests(test_labels)
 
         #save coverage report
         coverage.stop()
-        
-        modules = [module for name, module in sys.modules.items() \
-                       if module and any([name.startswith(label) for label in app_labels])]
 
+        modules = [module for name, module in sys.modules.items() \
+                       if module and any([name.endswith(label) for label in test_labels])]
+        print modules
         morfs = [ m.__file__ for m in modules if hasattr(m, '__file__') ]
         coverage._the_coverage.xml_report(morfs, outfile=path.join(output_dir,'coverage.xml'))
-
+    
     @staticmethod
-    def app_list():
-        if hasattr(settings, 'PROJECT_APPS'):
-            return settings.PROJECT_APPS
-        return settings.INSTALLED_APPS
-
+    def test_labels():
+        excludes = getattr(settings, 'TEST_EXCLUDES', [])
+        return [app for app in settings.INSTALLED_APPS if app not in excludes ]
