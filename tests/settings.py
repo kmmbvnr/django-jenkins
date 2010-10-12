8450036eaa168a09a0aac32ddc6d15f895d6141d
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2010-10-12 16:14:31 +0700

    Add app with dots in names to test suite

--- tests/settings.py
+++ tests/settings.py
@@ -1,2 +1,3 @@
-INSTALLED_APPS = ('django_hudson',)
+INSTALLED_APPS = ('django.contrib.sessions', # just to enshure that dotted apps test works
+                  'django_hudson',)
 DATABASE_ENGINE = 'sqlite3'
