23f88da47f4f65d779a9cb267ab6c7ee789c76d8
kmmbvnr <kmmbvnr@gmail.com>
2011-03-27 09:24:45 +0700

    Test mail stubs works

--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -1,23 +1,10 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
+from django.core import mail
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class SaintyChecks(TestCase):
+    def test_mailbox_stubs_not_broken(self):
+        mail.send_mail('Test subject', 'Test message', 'nobody@kenkins.com',
+                       ['somewhere@nowhere.com'])
+        self.assertTrue(1, len(mail.outbox))
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
