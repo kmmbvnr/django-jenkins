25f0d4fdb560c02c0f40e17c240dff717afe78bd
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2011-05-15 14:44:35 +0700

    Include missing jslint_runner.js, closes #29

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'pylint>=0.23',
     ],
     packages = ['django_jenkins', 'django_jenkins.management', 'django_jenkins.tasks', 'django_jenkins.management.commands'],
-    package_data={'django_jenkins': ['tasks/pylint.rc']},
+    package_data={'django_jenkins': ['tasks/pylint.rc', 'tasks/jslint_runner.js']},
     zip_safe = False,
     include_package_data = True
 )
