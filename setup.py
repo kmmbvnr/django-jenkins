178634b3e374b80ac104b48d3b3485cc714e5eaa
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2012-03-13 11:24:16 +0700

    Making submodules installable from git

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 import codecs
 from os import path
-from setuptools import setup
+from subprocess import check_call
+from distutils.core import setup
+from distutils.command.build import build
 
 read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
 
+class build_with_submodules(build):
+    def run(self):
+        check_call(['git', 'submodule', 'init'])
+        check_call(['git', 'submodule', 'update'])
+        build.run(self)
+
 setup(
     name = 'django-jenkins',
+    cmdclass={"build": build_with_submodules},
     version = '0.12.0',
     author = 'Mikhail Podgurskiy',
     author_email = 'kmmbvnr@gmail.com',
@@ -26,10 +35,10 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Testing'
     ],
-    install_requires=[
-        'Django>=1.3',
-        'coverage>=3.4',
-        'pylint>=0.23',
+    requires=[
+        'Django (>=1.3)',
+        'coverage (>=3.4)',
+        'pylint (>=0.23)',
     ],
     packages = ['django_jenkins', 'django_jenkins.management', 'django_jenkins.tasks', 'django_jenkins.management.commands'],
     package_data={'django_jenkins': ['tasks/pylint.rc', 'tasks/jslint_runner.js', 'tasks/jslint/jslint.js', 'tasks/csslint/release/csslint-rhino.js']},
