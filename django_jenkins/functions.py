ce300e605aa9119066f263c12020ed479cf4542a
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2012-05-04 13:40:03 +0700

    New unittest2 compatible test runner

--- django_jenkins/functions.py
+++ django_jenkins/functions.py
@@ -63,3 +63,10 @@ def find_first_existing_executable(exe_list):
         else:
             return filepath
 
+
+def total_seconds(delta):
+    """
+    Backport timedelta.total_seconds() from Python 2.7
+    """
+    return delta.days * 86400.0 + delta.seconds + delta.microseconds * 1e-6
+
