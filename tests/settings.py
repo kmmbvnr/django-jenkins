83c80d162008544ab5be5dd2005810c16087315b
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2013-01-20 20:33:44 +0700

    Enambe tox-driven testing

--- tests/settings.py
+++ tests/settings.py
@@ -37,7 +37,7 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.dir_tests',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_flake8',
+#    'django_jenkins.tasks.run_flake8',
     'django_jenkins.tasks.run_jshint',
     'django_jenkins.tasks.run_csslint',    
     'django_jenkins.tasks.run_sloccount',    
