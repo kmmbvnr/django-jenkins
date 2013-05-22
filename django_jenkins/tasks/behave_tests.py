6ba7a5c912e01e353838efd1461bae59602fb9a3
Scott Ferguson <scottwferg@gmail.com>
2013-05-22 15:09:58 -0500

    Move bulk of django_behave into the test runner, fix bug preventing it from running on the latest version of Behave

--- django_jenkins/tasks/behave_tests.py
+++ django_jenkins/tasks/behave_tests.py
@@ -1,12 +1,92 @@
 """
 Build suite with behave tests
 """
+from django.test.simple import build_suite
 from django.db.models import get_app
 from django.conf import settings
 from django_jenkins.tasks import BaseTask
+from django.test import LiveServerTestCase
 
-from django_behave.runner import get_features, DjangoBehaveTestCase, make_test_suite
+import unittest
+import sys
 
+from os.path import dirname, abspath, join, isdir
+
+import behave
+from behave.configuration import Configuration, ConfigError
+from behave.runner import Runner
+from behave.parser import ParserError
+from behave.formatter.ansi_escapes import escapes
+
+def get_features(app_module):
+    app_dir = dirname(app_module.__file__)
+    features_dir = abspath(join(app_dir, 'features'))
+    if isdir(features_dir):
+        return features_dir
+    else:
+        return None
+
+class DjangoBehaveTestCase(LiveServerTestCase):
+    def __init__(self, features_dir):
+        unittest.TestCase.__init__(self)
+        self.features_dir = features_dir
+        # sys.argv kludge
+        # need to understand how to do this better
+        # temporarily lose all the options etc
+        # else behave will complain
+        old_argv = sys.argv
+        sys.argv = old_argv[:2]
+        self.behave_config = Configuration()
+        sys.argv = old_argv
+        # end of sys.argv kludge
+        self.behave_config.paths = [features_dir]
+        self.behave_config.format = ['pretty']
+
+        self.behave_config.server_url = 'http://localhost:8081'
+
+        # disable these in case you want to add set_trace in the tests you're developing
+        self.behave_config.stdout_capture = False
+        self.behave_config.stderr_capture = False
+
+    def runTest(self, result=None):
+        # run behave on a single directory
+        print "run: features_dir=%s" % (self.features_dir)
+
+        # from behave/__main__.py
+        runner = Runner(self.behave_config)
+        try:
+            failed = runner.run()
+        except ParserError, e:
+            sys.exit(str(e))
+        except ConfigError, e:
+            sys.exit(str(e))
+
+        if self.behave_config.show_snippets and runner.undefined:
+            msg = u"\nYou can implement step definitions for undefined steps with "
+            msg += u"these snippets:\n\n"
+            printed = set()
+
+            if sys.version_info[0] == 3:
+                string_prefix = "('"
+            else:
+                string_prefix = u"(u'"
+
+            for step in set(runner.undefined):
+                if step in printed:
+                    continue
+                printed.add(step)
+
+                msg += u"@" + step.step_type + string_prefix + step.name + u"')\n"
+                msg += u"def impl(context):\n"
+                msg += u"    assert False\n\n"
+
+            sys.stderr.write(escapes['undefined'] + msg + escapes['reset'])
+            sys.stderr.flush()
+
+        self.assertFalse(failed)
+        
+def make_test_suite(features_dir):
+    return DjangoBehaveTestCase(features_dir=features_dir)
 
 class Task(BaseTask):
     def __init__(self, test_labels, options):
