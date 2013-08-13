1ad8dededecb340ded7ac1d9ec8e17ffef61a726
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2013-08-13 17:19:00 +0700

    Fix windmill tests

--- tests/test_app/urls.py
+++ tests/test_app/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-     url(r'^test_click/$', 'django.views.generic.simple.direct_to_template',
-         {'template': 'test_app/wm_test_click.html'}, name='wm_test_click')
+     url(r'^test_click/$', TemplateView.as_view(template_name='test_app/wm_test_click.html'), name='wm_test_click')
 )
