84c54a6b8eadb134cbed22afd84843c27eefe729
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2013-09-16 14:25:50 +0700

    Shitch to element tree xml generation

--- django_jenkins/runner.py
+++ django_jenkins/runner.py
@@ -1,255 +1,122 @@
 # -*- coding: utf-8; mode: django -*-
 import os
-import traceback
-from datetime import datetime
-from itertools import groupby
-from xml.sax.saxutils import XMLGenerator
-from xml.sax.xmlreader import AttributesImpl
+import sys
+import time
+
+from xml.etree import ElementTree as ET
 from django.conf import settings
 from django.test.simple import DjangoTestSuiteRunner
 from django.test.testcases import TestCase
 from django.utils.unittest import TestSuite, TextTestResult, TextTestRunner
-from django_jenkins import signals
-from django_jenkins.functions import total_seconds
 
 try:
     from django.test.simple import reorder_suite
 except ImportError:
     from django.test.runner import reorder_suite
 
-try:
-    from django.utils.encoding import smart_text
-except ImportError:
-    from django.utils.encoding import smart_unicode as smart_text
+from django_jenkins import signals
 
 
-STDOUT_LINE = '\nStdout:\n%s'
-STDERR_LINE = '\nStderr:\n%s'
-
-
-class TestInfo(object):
-    class RESULT(object):
-        SUCCESS = 0
-        ERROR = 1
-        FAILURE = 2
-        EXPECTED_FAILURE = 3
-        UNEXPECTED_SUCCESS = 4
-        SKIPPED = 5
-
-    __slots__ = ('method_name', 'case_name', 'failureException', 'start_time',
-                 'end_time', 'err', 'stdout', 'stderr', 'result', 'reason')
-
-    def __init__(self, **kwargs):
-        for slot_name in self.__slots__:
-            setattr(self, slot_name, None)
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-
-class XMLTestResult(TextTestResult):
-    """
-    Dumps xml junit output as well as text
-    """
+class EXMLTestResult(TextTestResult):
     def __init__(self, *args, **kwargs):
-        super(XMLTestResult, self).__init__(*args, **kwargs)
-        self.testInfos = []
-        self.currentTestInfo = None
-
-    def startTestRun(self):
-        """
-        Called once before any tests are executed.
-        """
-        super(XMLTestResult, self).startTestRun()
+        self.case_start_time = None
+        self.run_start_time = None
+        self.tree = None
+        super(EXMLTestResult, self).__init__(*args, **kwargs)
 
     def startTest(self, test):
-        """
-        Called when the given test is about to be run
-        """
-        self.currentTestInfo = TestInfo(
-            method_name=self.test_method_name(test),
-            case_name=self.test_case_name(test),
-            failureException=test.failureException, start_time=datetime.now()
-        )
-        super(XMLTestResult, self).startTest(test)
+        self.case_start_time = time.time()
+        super(EXMLTestResult, self).startTest(test)
 
-    def stopTest(self, test):
-        """
-        Called when the given test has been run
-        """
-        self.currentTestInfo.end_time = datetime.now()
-        self.currentTestInfo.stdout = self._stdout_buffer.getvalue()
-        self.currentTestInfo.stderr = self._stderr_buffer.getvalue()
-        self.testInfos.append(self.currentTestInfo)
-        super(XMLTestResult, self).stopTest(test)
-
-    def addError(self, test, err):
-        """
-        Called when an error has occurred. 'err' is a tuple of values as
-        returned by sys.exc_info()
-        """
-        self.currentTestInfo.result = TestInfo.RESULT.ERROR
-        self.currentTestInfo.err = err
-        super(XMLTestResult, self).addError(test, err)
-        signals.test_error.send(sender=self, test=test, err=err)
-
-    def addFailure(self, test, err):
-        """
-        Called when an error has occurred. 'err' is a tuple of values as
-        returned by sys.exc_info().
-        """
-        self.currentTestInfo.result = TestInfo.RESULT.FAILURE
-        self.currentTestInfo.err = err
-        super(XMLTestResult, self).addFailure(test, err)
-        signals.test_failure.send(sender=self, test=test, err=err)
+    def startTestRun(self):
+        self.tree = ET.Element('testsuite')
+        self.run_start_time = time.time()
+        super(EXMLTestResult, self).startTestRun()
 
     def addSuccess(self, test):
-        """
-        Called when a test has completed successfully
-        """
-        self.currentTestInfo.result = TestInfo.RESULT.SUCCESS
-        super(XMLTestResult, self).addSuccess(test)
-        signals.test_success.send(sender=self, test=test)
+        self.testcase = self._make_testcase_element(test)
+        super(EXMLTestResult, self).addSuccess(test)
 
-    def addSkip(self, test, reason):
-        """
-        Called when a test is skipped.
-        """
-        self.currentTestInfo.result = TestInfo.RESULT.SKIPPED
-        self.currentTestInfo.reason = reason
-        super(XMLTestResult, self).addSkip(test, reason)
+    def addFailure(self, test, err):
+        self.testcase = self._make_testcase_element(test)
+        test_result = ET.SubElement(self.testcase, 'failure')
+        self._add_tb_to_test(test, test_result, err)
+        super(EXMLTestResult, self).addFailure(test, err)
 
-    def addExpectedFailure(self, test, err):
-        """
-        Called when an expected failure/error occured.
-        """
-        self.currentTestInfo.result = TestInfo.RESULT.EXPECTED_FAILURE
-        self.currentTestInfo.err = err
-        super(XMLTestResult, self).addExpectedFailure(test, err)
-        signals.test_expected_failure.send(sender=self, test=test, err=err)
+    def addError(self, test, err):
+        self.testcase = self._make_testcase_element(test)
+        test_result = ET.SubElement(self.testcase, 'error')
+        self._add_tb_to_test(test, test_result, err)
+        super(EXMLTestResult, self).addError(test, err)
 
     def addUnexpectedSuccess(self, test):
-        """
-        Called when a test was expected to fail, but succeed.
-        """
-        self.currentTestInfo.result = TestInfo.RESULT.UNEXPECTED_SUCCESS
-        super(XMLTestResult, self).addUnexpectedSuccess(test)
-        signals.test_unexpected_success.send(sender=self, test=test)
+        self.testcase = self._make_testcase_element(test)
+        test_result = ET.SubElement(self.testcase, 'skipped')
+        test_result.set('message', 'Test Skipped: Unexpected Success')
+        super(EXMLTestResult, self).addUnexpectedSuccess(test)
 
-    def _exc_info_to_string(self, err, test):
-        """
-        Converts a sys.exc_info()-style tuple of values into a string.
-        """
-        exctype, value, tb = err
-        # Skip test runner traceback levels
-        while tb and self._is_relevant_tb_level(tb):
-            tb = tb.tb_next
-        if exctype is test.failureException:
-            # Skip assert*() traceback levels
-            length = self._count_relevant_tb_levels(tb)
-            msgLines = traceback.format_exception(exctype, value, tb, length)
-        else:
-            msgLines = traceback.format_exception(exctype, value, tb)
+    def addSkip(self, test, reason):
+        self.testcase = self._make_testcase_element(test)
+        test_result = ET.SubElement(self.testcase, 'skipped')
+        test_result.set('message', 'Test Skipped: %s' % reason)
+        super(EXMLTestResult, self).addSkip(test, reason)
 
+    def addExpectedFailure(self, test, err):
+        self.testcase = self._make_testcase_element(test)
+        test_result = ET.SubElement(self.testcase, 'skipped')
+        self._add_tb_to_test(test, test_result, err)
+        super(EXMLTestResult, self).addExpectedFailure(test, err)
+
+    def stopTest(self, test):
         if self.buffer:
-            if self._stdout_buffer:
-                output = self._stdout_buffer.getvalue()
-                if output:
-                    if not output.endswith('\n'):
-                        output += '\n'
-                    msgLines.append(STDOUT_LINE % output)
-            if self._stderr_buffer:
-                error = self._stderr_buffer.getvalue()
-                if error:
-                    if not error.endswith('\n'):
-                        error += '\n'
-                    msgLines.append(STDERR_LINE % error)
-        return ''.join(map(smart_text, msgLines))
+            output = sys.stdout.getvalue()
+            if output:
+                sysout = ET.SubElement(self.testcase, 'system-out')
+                sysout.text = output
 
-    def test_method_name(self, test):
-        """
-        Returns the test method name.
-        """
-        test_id = test.id()
-        return test_id.split('.')[-1]
+            error = sys.stderr.getvalue()
+            if error:
+                syserr = ET.SubElement(self.testcase, 'system-err')
+                syserr.text = error
 
-    def test_case_name(self, test):
-        """
-        Returns test case name
-        """
-        testcase = type(test)
-        module = testcase.__module__ + '.'
-        if module == '__main__.':
-            module = ''
-        return module + testcase.__name__
+        super(EXMLTestResult, self).stopTest(test)
+
+    def stopTestRun(self):
+        run_time_taken = time.time() - self.run_start_time
+        self.tree.set('name', 'Django Project Tests')
+        self.tree.set('errors', str(len(self.errors)))
+        self.tree.set('failures', str(len(self.failures)))
+        self.tree.set('skips', str(len(self.skipped)))
+        self.tree.set('tests', str(self.testsRun))
+        self.tree.set('time', "%.3f" % run_time_taken)
+        super(EXMLTestResult, self).stopTestRun()
+
+    def _make_testcase_element(self, test):
+        time_taken = time.time() - self.case_start_time
+        classname = ('%s.%s' % (test.__module__, test.__class__.__name__)).split('.')
+        testcase = ET.SubElement(self.tree, 'testcase')
+        testcase.set('time', "%.6f" % time_taken)
+        testcase.set('classname', '.'.join(classname))
+        testcase.set('name', test._testMethodName)
+        return testcase
+
+    def _add_tb_to_test(self, test, test_result, err):
+        '''Add a traceback to the test result element'''
+        exc_class, exc_value, tb = err
+        tb_str = self._exc_info_to_string(err, test)
+        test_result.set('type', '%s.%s' % (exc_class.__module__, exc_class.__name__))
+        test_result.set('message', str(exc_value))
+        test_result.text = tb_str
 
     def dump_xml(self, output_dir):
         """
         Dumps test result to xml
         """
-        self.buffer = False
-
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        with open(os.path.join(output_dir, 'junit.xml'), 'w') as output:
-            document = XMLGenerator(output, 'utf-8')
-            document.startDocument()
-            document.startElement('testsuites', AttributesImpl({}))
-
-            suites = groupby(self.testInfos,
-                             key=lambda test_info: test_info.case_name)
-            for suite_name, suite in suites:
-                document.startElement('testsuite',
-                                      AttributesImpl({'name': suite_name}))
-
-                for test_info in suite:
-                    document.startElement('testcase', AttributesImpl({
-                        'classname': suite_name,
-                        'name': test_info.method_name,
-                        'time': '%3f' % total_seconds(
-                                    test_info.end_time - test_info.start_time)
-                    }))
-
-                    if test_info.result == TestInfo.RESULT.ERROR:
-                        document.startElement('error', AttributesImpl({
-                            'message': smart_text(test_info.err[1])
-                        }))
-                        document.characters(self._exc_info_to_string(
-                                        test_info.err, test_info))
-                        document.endElement('error')
-                    elif test_info.result == TestInfo.RESULT.FAILURE:
-                        document.startElement('failure', AttributesImpl({
-                            'message': smart_text(test_info.err[1])
-                        }))
-                        document.characters(self._exc_info_to_string(
-                                        test_info.err, test_info))
-                        document.endElement('failure')
-                    elif test_info.result == \
-                                    TestInfo.RESULT.UNEXPECTED_SUCCESS:
-                        document.startElement('error', AttributesImpl({
-                            'message': 'Unexpected success'
-                        }))
-                        document.endElement('error')
-                    elif test_info.result == TestInfo.RESULT.SKIPPED:
-                        document.startElement('skipped', AttributesImpl({}))
-                        document.characters(test_info.reason)
-                        document.endElement('skipped')
-
-                    if test_info.stdout:
-                        document.startElement('system-out', AttributesImpl({}))
-                        document.characters(test_info.stdout)
-                        document.endElement('system-out')
-
-                    if test_info.stderr:
-                        document.startElement('system-err', AttributesImpl({}))
-                        document.characters(test_info.stderr)
-                        document.endElement('system-err')
-                    document.endElement('testcase')
-
-                document.endElement('testsuite')
-
-            document.endElement('testsuites')
-            document.endDocument()
+        output = ET.ElementTree(self.tree)
+        output.write(os.path.join(output_dir, 'junit.xml'), encoding="utf-8")
 
 
 class CITestSuiteRunner(DjangoTestSuiteRunner):
@@ -272,8 +139,8 @@ class CITestSuiteRunner(DjangoTestSuiteRunner):
     def setup_databases(self):
         if 'south' in settings.INSTALLED_APPS:
             from south.management.commands import (
-                    patch_for_test_db_setup  # pylint: disable=F0401
-                    )
+                patch_for_test_db_setup  # pylint: disable=F0401
+            )
             patch_for_test_db_setup()
         return super(CITestSuiteRunner, self).setup_databases()
 
@@ -285,9 +152,8 @@ class CITestSuiteRunner(DjangoTestSuiteRunner):
     def run_suite(self, suite, **kwargs):
         signals.before_suite_run.send(sender=self)
         result = TextTestRunner(buffer=True,
-                resultclass=XMLTestResult,
-                verbosity=self.verbosity
-                ).run(suite)
+                                resultclass=EXMLTestResult,
+                                verbosity=self.verbosity).run(suite)
         if self.with_reports:
             result.dump_xml(self.output_dir)
         signals.after_suite_run.send(sender=self)
