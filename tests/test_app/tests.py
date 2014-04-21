512fad4677148b037a254e4567d45cef7ea0c735
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-04-21 16:29:50 +0700

    Add selenium to p3k suite

--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -4,6 +4,7 @@ from django.core import mail
 from django.test import TestCase
 from django.utils.unittest import skip
 from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class SaintyChecks(TestCase):
@@ -34,27 +35,20 @@ class SaintyChecks(TestCase):
     #    raise Exception("Ups, should be disabled")
 
 
-if sys.version_info[0] < 3:
-    """
-    selenium not works on p3k yet
-    """
-    from selenium.webdriver.firefox.webdriver import WebDriver
+class SeleniumTests(LiveServerTestCase):
+    fixtures = ['default_users.json']
 
-    class SeleniumTests(LiveServerTestCase):
-        fixtures = ['default_users.json']
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(SeleniumTests, cls).setUpClass()
 
-        @classmethod
-        def setUpClass(cls):
-            cls.selenium = WebDriver()
-            super(SeleniumTests, cls).setUpClass()
+    @classmethod
+    def tearDownClass(cls):
+        super(SeleniumTests, cls).tearDownClass()
+        cls.selenium.quit()
 
-        @classmethod
-        def tearDownClass(cls):
-            super(SeleniumTests, cls).tearDownClass()
-            cls.selenium.quit()
-
-        def test_login(self):
-            self.selenium.get('%s%s' % (self.live_server_url, '/test_click/'))
-            self.selenium.find_element_by_id("wm_click").click()
-            self.assertEqual('Button clicked', self.selenium.find_element_by_id("wm_target").text)
-
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/test_click/'))
+        self.selenium.find_element_by_id("wm_click").click()
+        self.assertEqual('Button clicked', self.selenium.find_element_by_id("wm_target").text)
