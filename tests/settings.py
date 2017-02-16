f673c8ecded24eb56aeaef8d1c0caeb82208b595
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-02-16 18:03:00 +0600

    Update deps

--- tests/settings.py
+++ tests/settings.py
@@ -60,12 +60,8 @@ SOUTH_MIGRATION_MODULES = {
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_scsslint',
-    'django_jenkins.tasks.run_flake8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jshint',
-    'django_jenkins.tasks.run_sloccount',
+    'django_jenkins.tasks.run_flake8',
 )
 
 COVERAGE_EXCLUDES = ['tests.test_app.not_for_coverage', ]
